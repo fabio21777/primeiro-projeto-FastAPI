3d96bac266c064e2926900ab5d99fd24c425bd21
Fabio <fabio@itexto.com.br>
2023-07-05 20:35:02 -0300

    corpo de requisição

--- projeto/main.py
+++ projeto/main.py
@@ -1,30 +1,31 @@
-from enum import Enum
+from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
-class ModelName(str, Enum):
-    alexnet = "alexnet descrição"
-    resnet = "resnet descrição"
-    lenet = "lenet descrição"
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    print(model_name)
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
---
+++ projeto/parametro de url/main-1.py
@@ -0,0 +1,61 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet descrição"
+    resnet = "resnet descrição"
+    lenet = "lenet descrição"
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+""" @app.get("/items/{item_id}")
+async def read_item(item_id:int):
+    return {"item_id": item_id} """
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    print(model_name)
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+""" @app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit] """
+
+
+""" @app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id} """
+
+""" @app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item """
+
+"não atribuir valor default torna o parametro obrigatório"
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
