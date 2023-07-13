5cc24ad07ad56edb9966e1ceb4e0a5ec3a397c86
fabio21777 <fabiosouza21777@gmail.com>
2023-07-13 20:40:10 -0300

    Body - Fields

--- projeto/main.py
+++ projeto/main.py
@@ -1,54 +1,21 @@
 from typing import Annotated
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
-
-@app.put("/items1/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Item,
-    user: User,
-    importance: Annotated[int, Body(gt=0)],
-    q: str | None = None,
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    if q:
-        results.update({"q": q})
-    return results
-
-from typing import Annotated
-
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-@app.put("/items2/{item_id}")
+@app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
-
